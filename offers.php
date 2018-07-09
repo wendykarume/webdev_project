939456c432fcd937e2c494bbb8f9c1fa3762a91f
EffyOkoth <31475952+EffyOkoth@users.noreply.github.com>
2018-07-09 14:53:10 +0300

    Add files via upload

---
+++ offers.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html/>
+<html>
+    
+<head>
+    <title>Our Offers</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="hero-image">
+        <a href="home.html"><button class="button"><i>Home</i></button></a>
+        <a href="offers.php" ><button class="button"><i>Offers</i></button></a>
+        <a href="about.php"><button class="button"><i>About</i></button></a>  
+        <a href="contact.php"><button class="button"><i>Contact Us</i></button></a>  
+     <div class="hero-text">
+        <h1 style="font-size:40px"> BlackRose Catering Services</h1>
+        <p><h2 style="font-size:160%">Where tasteful creations begin </h2></p>
+        <a href="order.php"><button>Place Order</button></a>
+      </div>
+  </div>b
+  <br>
+    <table style="width:80%" align="center">
+  <tr>
+    <td><img src="http://www.tamarind.co.ke/images/food-menu/soup.jpg" height="300" width="400"></td>
+    <td align="center"><h2>Freshly Prepared Soups</h2><br> Soup of the day with homemade brown bread.</td>
+ 
+    
+  </tr>
+  <tr>
+    <td align="center"><h2>Salads</h2><br>They incude mixed salad and rice salad.</td>
+    <td><img src="http://www.tamarind.co.ke/images/food-menu/salad.jpg" height="300" width="400" align="right"></td>
+   
+  </tr>
+  <tr>
+    <td> <img src="http://www.tamarind.co.ke/images/food-menu/big_plate.jpg" height="300" width="400"></td>
+    <td align="center"><h2>Meat</h2><br>Leg of beef,Crocodile,Ostrich,Leg of pork,Leg of lamb,Rabbit,Pork Spare Ribs,Chicken Tikka,Chicken Yakitori,Chicken wings,Chicken livers and gizzards.</td>  
+  </tr>
+  <tr>
+    <td align="center"><h2>Dessert</h2><br>Fresh Fruit Sorbet,Italian ice cream,Fresh Fruit Salad,Passion Cheese Cake,Choc-Chip Blondie,Vanilla Creme Brulee,Kenya Coffee.</td>
+    <td> <img src="http://www.tamarind.co.ke/images/food-menu/dessert.jpg" height="300" width="400" align="right"></td>
+  </tr>
+  <tr>
+    <td><img src="http://www.tamarind.co.ke/images/food-menu/wine.jpg" height="300" width="400"></td>
+    <td align="center"><h2>Wine List</h2><br>Indomito,Chamdor,Tamarind white,House White,Graham Beck Chardonnay,Sunrise Chardonnay,Orvieto Classico.</td>
+  </tr>
+  <tr>
+    <td align="center"><h2>Cocktails</h2><br>Martinis,Tequila Sunrise,Bloody Mary,Rum coladas,Mojito,Daawa.</td>
+    <td><img src="http://www.tamarind.co.ke/images/food-menu/cocktail2.jpg" height="300" width="400" align="right"></td>
+  </tr>
+</table>
+<br>
+  <a href="#" class="fa fa-phone"></a> +254702037288 or +254740804376 &nbsp;
+  <a href="#" class="fa fa-google"></a> blackrose@caterers.com &nbsp; &nbsp;
+  <a href="#" class="fa fa-twitter"></a> @ __blackrosecaterers_ &nbsp; &nbsp;
+  <a href="#" class="fa fa-snapchat-ghost"></a> @__blackrose_catering254  &nbsp;</br>
+  <a href="#" class="fa fa-facebook"></a> @__blackrose_caterers  &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+  <a href="#" class="fa fa-instagram"></a> @__blackrosecaterers254   &nbsp; 
+  <a href="#" class="fa fa-pinterest"></a> @__blackrosecaterers254  &nbsp; 
+  <a href="#" class="fa fa-tumblr"></a> @__blackrose_caterers254
+</body>
+</html>
